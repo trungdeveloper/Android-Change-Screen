c677df78b240a302d08b92a2f31b77e00a6e7d2a
Nguyen Tan Trung <trung.nguyen@pn.com.vn>
2019-09-16 08:24:50 +0700

    bla

---
+++ app/src/main/java/com/example/androidchangescreen/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.androidchangescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
